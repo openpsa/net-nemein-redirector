bc9a3415a19d9bd5b2f982e5f7516f726929ae52
flack <flack@contentcontrol-berlin.de>
2024-05-20 12:54:55 +0000

    - Convert to yml routes (8a97b6af5)  - apply metatag configuration to tinyurls, too (991cee2b4)

--- lib/net/nemein/redirector/handler/redirect.php
+++ lib/net/nemein/redirector/handler/redirect.php
@@ -6,6 +6,9 @@
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
 
+use midcom\datamanager\datamanager;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * @package net.nemein.redirector
  */
@@ -14,10 +17,10 @@ class net_nemein_redirector_handler_redirect extends midcom_baseclasses_componen
     /**
      * Process the redirect request
      */
-    public function _handler_redirect(array $args)
+    public function _handler_redirect(string $tinyurl)
     {
         $mc = net_nemein_redirector_tinyurl_dba::new_collector('node', $this->_topic->guid);
-        $mc->add_constraint('name', '=', $args[0]);
+        $mc->add_constraint('name', '=', $tinyurl);
         $mc->add_value_property('code');
         $mc->add_value_property('url');
         $mc->execute();
@@ -26,33 +29,26 @@ class net_nemein_redirector_handler_redirect extends midcom_baseclasses_componen
 
         // No results found
         if (empty($results)) {
-            throw new midcom_error_notfound($args[0] . ' could not be found');
+            throw new midcom_error_notfound($tinyurl . ' could not be found');
         }
 
         // Catch first the configuration option for showing editing interface instead
         // of redirecting administrators
         if (   $this->_topic->can_do('net.nemein.redirector:noredirect')
             && !$this->_config->get('admin_redirection')) {
-            return new midcom_response_relocate("edit/{$args[0]}/");
+            return new midcom_response_relocate($this->router->generate('edit', ['tinyurl' => $tinyurl]));
         }
         $guid = key($results);
-        $url = $mc->get_subkey($guid, 'url');
-        $code = $mc->get_subkey($guid, 'code') ?: $this->_config->get('redirection_code');
 
-        return new midcom_response_relocate($url, $code);
+        return $this->redirect($mc->get_subkey($guid, 'url'), $mc->get_subkey($guid, 'code') ?: null);
     }
 
-    /**
-     * Process the index request
-     */
-    public function _handler_index(array &$data)
+    private function redirect(string $url, ?int $code = null) : Response
     {
-        // Get the topic link and relocate accordingly
-        $url = net_nemein_redirector_viewer::topic_links_to($this->_config);
-
         if (!$this->_config->get('redirection_metatag')) {
-            return new midcom_response_relocate($url, $this->_config->get('redirection_code'));
+            return new midcom_response_relocate($url, $code ?: $this->_config->get('redirection_code'));
         }
+
         // Metatag redirection
         $data['redirection_url'] = $url;
         $data['redirection_speed'] = $this->_config->get('redirection_metatag_speed');
@@ -60,8 +56,50 @@ class net_nemein_redirector_handler_redirect extends midcom_baseclasses_componen
         midcom::get()->head->add_meta_head([
             'http-equiv' => 'refresh',
             'content' => "{$data['redirection_speed']};url={$url}",
-        ]);
+            ]);
 
         return $this->show('redirection-page');
+    }
+
+    /**
+     * Process the index request
+     */
+    public function _handler_index(array &$data)
+    {
+        $data['url'] = net_nemein_redirector_viewer::topic_links_to($this->_config);
+
+        if (   $this->_config->get('redirection_type') !== null
+            && (   !$this->_topic->can_do('net.nemein.redirector:noredirect')
+                || $this->_config->get('admin_redirection'))) {
+            return $this->redirect($data['url']);
+        }
+
+        // List TinyURLs
+        $qb = net_nemein_redirector_tinyurl_dba::new_query_builder();
+        $qb->add_constraint('node', '=', $this->_topic->guid);
+
+        $data['tinyurls'] = $qb->execute();
+        $data['workflow'] = $this->get_workflow('datamanager');
+    }
+
+    /**
+     * Show the list of TinyURLs
+     */
+    public function _show_index(string $handler_id, array &$data)
+    {
+        midcom_show_style('tinyurl-list-start');
+
+        // Initialize the datamanager instance
+        $datamanager = datamanager::from_schemadb($this->_config->get('schemadb_tinyurl'));
+
+        foreach ($data['tinyurls'] as $tinyurl) {
+            $data['tinyurl'] = $tinyurl;
+            $datamanager->set_storage($tinyurl);
+            $data['view_tinyurl'] = $datamanager->get_content_html();
+
+            midcom_show_style('tinyurl-list-item');
+        }
+
+        midcom_show_style('tinyurl-list-end');
     }
 }
